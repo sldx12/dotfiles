dedcf9948710e603330de2b34aee392cdd7e0cd3
sldx12 <63618062+sldx12@users.noreply.github.com>
2020-12-07 18:24:36 +0000

    removed comment

--- .config/i3blocks/blocks/battery.sh
+++ .config/i3blocks/blocks/battery.sh
@@ -3,8 +3,6 @@
 BATTERY=$(cat /sys/class/power_supply/BAT0/capacity)
 STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/state/ {print $2}')
 
-# 0 - 10 / 10 - 30 / 30 - 60 / 60 - 90 / 90 - 100
-
 if [[ "$STATE" = "charging" ]]; then
 	echo "" "$BATTERY%"
 elif [[ "$STATE" = "discharging" ]]; then
