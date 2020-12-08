e1a1d4e2c35122626ad4cab36c72448c1a263383
sldx12 <63618062+sldx12@users.noreply.github.com>
2020-12-08 00:20:31 +0000

    fix for when volume is muted

--- .config/i3blocks/blocks/volume.sh
+++ .config/i3blocks/blocks/volume.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+ONOFF=$(amixer get Master | awk '/Front Left:/ {print $6}')
 VOLUME=$(amixer get Master | awk '/Front Left:/ {print $5}')
 VNUM=${VOLUME:1:-2}
 
-if [[ $VNUM = 0% ]]; then
-	echo "$VNUM"
-elif [[ $VNUM -ge 50 ]]; then 
-	echo " $VNUM"
-elif [[ $VNUM -ge 0 ]]; then
-	echo "$VNUM" 
+if [[ "$ONOFF" = "[off]" ]]; then
+	echo " off"
+else
+	if [[ $VNUM = 0% ]]; then
+		echo "$VNUM%"
+	elif [[ $VNUM -ge 50 ]]; then 
+		echo " $VNUM%"
+	elif [[ $VNUM -ge 0 ]]; then
+		echo "$VNUM%" 
+	fi
 fi
