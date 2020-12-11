4f8cc1654e2962463a4fc9a5df614762e3be875d
sldx12 <63618062+sldx12@users.noreply.github.com>
2020-12-11 09:42:25 +0000

    small fix

--- .config/i3blocks/blocks/volume.sh
+++ .config/i3blocks/blocks/volume.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ONOFF=$(amixer get Master | awk '/Front Left:/ {print $6}')
+ONOFF2=$(amixer get Master | awk '/Mono:/ {print $6}')
 VOLUME=$(amixer get Master | awk '/Front Left:/ {print $5}')
 VNUM=${VOLUME:1:-2}
 
-if [[ "$ONOFF" = "[off]" ]]; then
+if [ "$ONOFF" = "[off]" ] || [ "$ONOFF2" == "[off]" ]; then
 	echo " off"
 else
 	if [[ $VNUM = 0% ]]; then
